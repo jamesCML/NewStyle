9d8163724eacf2a0e389aace894e8d752029a01e
yaofang <1049305130@qq.com>
2018-08-02 16:12:39 +0800

    1.补充文件

---
+++ app/src/main/java/com/uubox/adapters/BigKeyConfigAd.java
@@ -0,0 +1,114 @@
+package com.uubox.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.uubox.padtool.R;
+import com.uubox.tools.IniAdapter;
+import com.uubox.tools.SimpleUtil;
+
+public class BigKeyConfigAd extends BaseAdapter {
+    private List<BigKeyConfigItemObj> mData;
+    private Context mContext;
+    private IBigKeyConfigClick mIBigKeyConfigClick;
+
+    public BigKeyConfigAd(Context context, List<BigKeyConfigItemObj> data) {
+        mContext = context;
+        mData = data;
+    }
+
+    public void setmIBigKeyConfigClick(IBigKeyConfigClick iBigKeyConfigClick) {
+        mIBigKeyConfigClick = iBigKeyConfigClick;
+    }
+
+    @Override
+    public int getCount() {
+        return mData.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mData.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup group) {
+        Holder holder;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.bigkeyconfigitem, null);
+            TextView tv = convertView.findViewById(R.id.bigkeyconfigitem_tv);
+            ImageView img = convertView.findViewById(R.id.bigkeyconfigitem_img);
+            ListView listview = convertView.findViewById(R.id.bigkeyconfigitem_list);
+
+
+            holder = new Holder();
+            holder.img = img;
+            holder.tv = tv;
+            holder.listView = listview;
+            convertView.setTag(holder);
+
+        } else {
+            holder = (Holder) convertView.getTag();
+        }
+
+        holder.tv.setText(mData.get(position).mTv);
+        holder.img.setImageResource(mData.get(position).mImgState ? R.mipmap.down_arrow : R.mipmap.right_arrow);
+
+        if (mData.get(position).mImgState) {
+            holder.img.setImageResource(R.mipmap.down_arrow);
+            holder.listView.setVisibility(View.VISIBLE);
+            final IniAdapter iniAdapter = new IniAdapter(mContext, mData.get(position).mSubData);
+            holder.listView.setAdapter(iniAdapter);
+            holder.listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position2, long id) {
+                    SimpleUtil.log("subItem:" + mData.get(position).mSubData.get(position2).name + "  size:" + mData.get(position).mSubData.size());
+                    mIBigKeyConfigClick.onItemClick(position, position2, iniAdapter);
+                }
+            });
+            holder.listView.measure(0, 0);
+            LinearLayout.LayoutParams param = (LinearLayout.LayoutParams) holder.listView.getLayoutParams();
+            param.height = holder.listView.getMeasuredHeight() * mData.get(position).mSubData.size();
+            holder.listView.setLayoutParams(param);
+        } else {
+            holder.img.setImageResource(R.mipmap.right_arrow);
+            holder.listView.setVisibility(View.GONE);
+        }
+
+
+        return convertView;
+    }
+
+    class Holder {
+        TextView tv;
+        ImageView img;
+        ListView listView;
+    }
+
+    public static class BigKeyConfigItemObj {
+        public String mTv;
+        //false:down true:right
+        public boolean mImgState;
+        public List<IniAdapter.IniObj> mSubData = new ArrayList<>();
+    }
+
+    public interface IBigKeyConfigClick {
+        void onItemClick(int mainPosition, int subPosition, IniAdapter adapter);
+    }
+}
---
+++ app/src/main/java/com/uubox/views/MouseAdjestDialog.java
@@ -0,0 +1,127 @@
+package com.uubox.views;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import com.uubox.padtool.R;
+import com.uubox.tools.SimpleUtil;
+
+/**
+ * 摇杆模式
+ * Created by Yj on 2015/8/19.
+ */
+public class MouseAdjestDialog implements View.OnClickListener, SeekBar.OnSeekBarChangeListener {
+
+
+    private static final String TAG = "MouseAdjestDialog";
+    /**
+     * <p>鼠标灵敏度 X,Y 轴</p>
+     * <p>mSeekBarY,mSeekBarX 改用 mSeekBarMouse 代替</p>
+     */
+    private SeekBar mSeekBarMouse, mSeekBarW;
+    /**
+     * 鼠标灵敏度-文本 X,Y 轴
+     */
+    private TextView mTextMouseSensitivityX, mTextMouseSensitivityW;
+
+    /**
+     * 选择使用鼠标右键跳出鼠标指针(Is: 右键 ，No：中键)
+     */
+    private RadioButton mIsMouseRight;
+
+    private View mView;
+    private Context mContext;
+
+    private int mInitmousesen;
+    private int mInitmousesrcollsen;
+
+    public View create(Context context) {
+        KeyboardEditWindowManager.getInstance().hideOrShowBottomUIMenu(false);
+        mContext = context;
+        mView = LayoutInflater.from(mContext).inflate(R.layout.dialog_keyboard_rock_attribute_edit, null);
+        initView();
+        return mView;
+    }
+
+    private void initView() {
+        initRockView();
+        setRockListener();
+        mInitmousesen = (Integer) SimpleUtil.getFromShare(mContext, "ini", "mousesen", int.class, 10);
+        mInitmousesrcollsen = (Integer) SimpleUtil.getFromShare(mContext, "ini", "mousesrcollsen", int.class, 20);
+
+        mSeekBarMouse.setProgress(mInitmousesen);
+        mSeekBarW.setProgress(mInitmousesrcollsen);
+        mTextMouseSensitivityX.setText("鼠标灵敏度:" + mInitmousesen);
+        mTextMouseSensitivityW.setText("滚轮灵敏度:" + mInitmousesrcollsen);
+
+
+    }
+
+    private void initRockView() {
+        mSeekBarMouse = mView.findViewById(R.id.sbar_mouse_sensitivity_x);
+        mTextMouseSensitivityX = mView.findViewById(R.id.tv_mouse_sensitivity_x);
+        mSeekBarW = mView.findViewById(R.id.sbar_mouse_Wheel);
+        mTextMouseSensitivityW = mView.findViewById(R.id.tv_mouse_wheel);
+        mIsMouseRight = mView.findViewById(R.id.rbn_mouse_right);
+
+    }
+
+    private void setRockListener() {
+        mSeekBarMouse.setOnSeekBarChangeListener(this);
+        mSeekBarW.setOnSeekBarChangeListener(this);
+        mView.findViewById(R.id.rbn_mouse_save).setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+            case R.id.rbn_mouse_save:
+                KeyboardEditWindowManager.getInstance().hideOrShowBottomUIMenu(true);
+                KeyboardEditWindowManager.getInstance().removeTop();
+
+                int aftermousesen = (Integer) SimpleUtil.getFromShare(mContext, "ini", "mousesen", int.class, 10);
+                int aftermousesrcollsen = (Integer) SimpleUtil.getFromShare(mContext, "ini", "mousesrcollsen", int.class, 20);
+                if (aftermousesen != mInitmousesen || aftermousesrcollsen != mInitmousesrcollsen) {
+                    SimpleUtil.notifyall_(10003, null);
+                }
+                break;
+        }
+
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        switch (seekBar.getId()) {
+            case R.id.sbar_mouse_sensitivity_x:
+                mTextMouseSensitivityX.setText("鼠标灵敏度:" + progress);
+                break;
+            case R.id.sbar_mouse_Wheel:
+                mTextMouseSensitivityW.setText("滚轮灵敏度:" + progress);
+                break;
+        }
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+        Log.e("------------", "开始滑动！");
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        SimpleUtil.saveToShare(mContext, "ini", "mousesen", mSeekBarMouse.getProgress());
+        SimpleUtil.saveToShare(mContext, "ini", "mousesrcollsen", mSeekBarW.getProgress());
+        SimpleUtil.addMsgBottomToTop(mContext, "修改成功！", false);
+    }
+
+
+}
